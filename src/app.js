5294cf577b17ccb450e018e1469992fca6ebd36c
Daniela-MV <danmv.0991@gmail.com>
2025-03-23 14:32:00 +0000

    Ya casi

--- src/app.js
+++ src/app.js
@@ -8,24 +8,28 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let extension = ['.com', '.net'];
-
-let dominios = pronoun + adj+ noun + extension;
-// for (let i = 0; i < pronoun.length; i++) {
-// for (let j=0; j <adj.length; j++){
-// }
-// for (let k = 0; k < noun.length; k++){
-// }
-// for (let l = 0; l < extension.length; l++){
-//   return i,j,k,l
+function nombresDom(){
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = ['.com', '.net'];
   
-// }
+  
+  for (let i = 0; i < pronoun.length; i++){
+    for (let j = 0; i < adj.length; j++){
+      for (let k = 0; k < noun.length; k++){
+        for (let l = 0; l < extension.length; l++){
+          return pronoun[i] + adj[j] + noun[k] + extension[l]
+        }
+      }
+    }
+  }
+  
 
-// }
-console.log(dominios);
+}
+console.log(nombresDom());
+   
+    
 
   }
   
