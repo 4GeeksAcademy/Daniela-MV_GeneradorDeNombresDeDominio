1977db23a0cb6fdb505f6e621588fefec673e316
Daniela-MV <danmv.0991@gmail.com>
2025-03-23 20:43:33 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -8,29 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-function nombresDom(){
+
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
   let extension = ['.com', '.net'];
-  
+
   
   for (let i = 0; i < pronoun.length; i++){
-    for (let j = 0; i < adj.length; j++){
+    for (let j = 0; j < adj.length; j++){
       for (let k = 0; k < noun.length; k++){
         for (let l = 0; l < extension.length; l++){
-          return pronoun[i] + adj[j] + noun[k] + extension[l]
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+           
+           
         }
       }
     }
   }
   
-
-}
-console.log(nombresDom());
-   
-    
-
   }
   
 
